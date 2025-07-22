7ecc461b481e6b28528d6f9bada871ae9d5bb5f4
176409 <148116817+176409@users.noreply.github.com>
2025-07-22 23:53:37 +0600

    first commit

---
+++ src/app/components/Services.jsx
@@ -0,0 +1,49 @@
+// app/services/page.jsx or page.tsx
+
+import dbConnect from "@/lib/dbConnect";
+import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
+const ServicesPage = async () => {
+  const services = await dbConnect('text_services').find().toArray()
+  console.log(services);
+
+  return (
+    <div className="w-11/12 mx-auto overflow-hidden">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 items-center justify-center w-11/12 mx-auto">
+        {services?.map((service) => (
+          <div
+            key={service?._id}
+            className=" rounded-xl border p-4 shadow-sm hover:shadow-md transition duration-300 bg-white overflow-hidden"
+          >
+            {/* Image */}
+            <div className="w-full aspect-video relative rounded-lg overflow-hidden">
+              <Image
+                src={service.img}
+                alt={service.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            {/* Title */}
+            <h1 className="text-lg md:text-xl font-semibold mt-4">
+              {service.title}
+            </h1>
+
+            {/* Price */}
+            <p className="text-[#FF3811] font-semibold mt-1">
+              Price : ${service.price}
+            </p>
+
+            {/* Arrow Icon */}
+            <div className="mt-2 flex justify-end">
+              <FaArrowRight className="text-[#FF3811] text-base md:text-lg" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ServicesPage;
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import ServicesPage from "@/app/components/Services";
+
+export default function Home() {
+  return (
+    <div className="">
+      <ServicesPage></ServicesPage>;
+    </div>
+  );
+}
